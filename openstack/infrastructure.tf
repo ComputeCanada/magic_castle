621dc18eb1e8c589568c16f2fed6e7ea3ee99f1a
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-03-28 09:59:00 -0400

    Create a Terraform folder for OpenStack

---
+++ openstack/infrastructure.tf
@@ -0,0 +1,123 @@
+provider "openstack" {
+  user_name   = "${var.os_username}"
+  password    = "${var.os_password}"
+  auth_url    = "${var.os_auth_url}"
+  tenant_name = "${var.os_tenant_name}"
+  tenant_id   = "${var.os_tenant_id}"
+  region      = "${var.os_region_name}"
+}
+
+data "template_file" "common" {
+  template = "${file("common.yaml")}"
+
+  vars {
+    nb_nodes      = "${var.nb_nodes}"
+    compute_vcpus = "${var.compute_vcpus}"
+    compute_ram   = "${var.compute_ram - 256}"
+    compute_disk  = "${var.compute_disk}"
+  }
+}
+
+data "template_file" "login" {
+  template = "${file("login.yaml")}"
+
+  vars {
+    admin_passwd = "${var.admin_passwd}"
+    guest_passwd = "${var.guest_passwd}"
+    nb_users     = "${var.nb_users}"
+    nb_nodes     = "${var.nb_nodes}"
+  }
+}
+
+data "template_cloudinit_config" "login_config" {
+  part {
+    filename     = "login.yaml"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.login.rendered}"
+  }
+
+  part {
+    filename     = "common.yaml"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.common.rendered}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "login1" {
+  name            = "login1"
+  flavor_id       = "${var.os_login_flavor_id}"
+  key_pair        = "${var.os_ssh_key}"
+  security_groups = ["default", "ssh"]
+  user_data       = "${data.template_cloudinit_config.login_config.rendered}"
+
+  block_device {
+    uuid                  = "${var.os_image_id}"
+    source_type           = "image"
+    volume_size           = 20
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
+
+  network {
+    name = "${var.os_default_network}"
+  }
+}
+
+data "template_file" "node" {
+  template = "${file("node.yaml")}"
+
+  vars {
+    admin_passwd = "${var.admin_passwd}"
+    login1_ip    = "${openstack_compute_instance_v2.login1.network.0.fixed_ip_v4}"
+  }
+}
+
+data "openstack_compute_flavor_v2" "node" {
+  vcpus = "${var.compute_vcpus}"
+  ram   = "${var.compute_ram}"
+  disk  = "${var.compute_disk}"
+}
+
+data "template_cloudinit_config" "node_config" {
+  part {
+    filename     = "node.yaml"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.node.rendered}"
+  }
+
+  part {
+    filename     = "common.yaml"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.common.rendered}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "node" {
+  count    = "${var.nb_nodes}"
+  name     = "compute_node${count.index + 1}"
+  image_id = "${var.os_image_id}"
+
+  # flavor_id       = "${var.os_flavor_id}"
+  flavor_id       = "${data.openstack_compute_flavor_v2.node.id}"
+  key_pair        = "${var.os_ssh_key}"
+  security_groups = ["default"]
+  user_data       = "${data.template_cloudinit_config.node_config.rendered}"
+
+  network {
+    name = "${var.os_default_network}"
+  }
+}
+
+resource "openstack_networking_floatingip_v2" "fip_1" {
+  pool = "${var.os_external_network}"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+  floating_ip = "${openstack_networking_floatingip_v2.fip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.login1.id}"
+}
+
+output "ip" {
+  value = "${openstack_networking_floatingip_v2.fip_1.address}"
+}
---
+++ openstack/variables.tf
@@ -0,0 +1,39 @@
+variable "nb_nodes" {}
+
+variable "nb_users" {}
+
+variable "admin_passwd" {}
+
+variable "guest_passwd" {}
+
+variable "compute_vcpus" {}
+
+variable "compute_ram" {}
+
+variable "compute_disk" {}
+
+variable "os_username" {}
+
+variable "os_password" {}
+
+variable "os_ssh_key" {}
+
+variable "os_external_network" {}
+
+variable "os_tenant_name" {}
+
+variable "os_default_network" {}
+
+variable "os_tenant_id" {}
+
+variable "os_project_name" {}
+
+variable "os_flavor_id" {}
+
+variable "os_login_flavor_id" {}
+
+variable "os_image_id" {}
+
+variable "os_auth_url" {}
+
+variable "os_region_name" {}
