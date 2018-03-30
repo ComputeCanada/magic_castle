41d3b68c04c8ee574b612932b47538325a0e73d0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-03-30 16:52:53 -0400

    Commit cluster variables definition

--- openstack/variables.tf
+++ openstack/variables.tf
@@ -1,39 +1,47 @@
-variable "nb_nodes" {}
+variable "nb_nodes" {
+  default = 5
+}
 
-variable "nb_users" {}
+variable "nb_users" {
+  default = 20
+}
 
-variable "admin_passwd" {}
+variable "shared_storage_size" {
+  default = 50
+}
 
-variable "guest_passwd" {}
+variable "admin_passwd" {
+  default = "changeme"
+}
 
-variable "compute_vcpus" {}
+variable "guest_passwd" {
+  default = "UQTR20180329"
+}
 
-variable "compute_ram" {}
+variable "compute_vcpus" {
+  default = 2
+}
 
-variable "compute_disk" {}
+variable "compute_ram" {
+  default = 7680
+}
 
-variable "os_username" {}
+variable "compute_disk" {
+  default = 20
+}
 
-variable "os_password" {}
+variable "os_flavor_id" {
+  default = "2ff7463c-dda9-4687-8b7a-80ad3303fd41"
+}
 
-variable "os_ssh_key" {}
+variable "os_login_flavor_id" {
+  # p2-3gb
+  # default = "2ff7463c-dda9-4687-8b7a-80ad3303fd41"
+  # c4-15gb-31
+  default = "9493fdd3-3100-440d-a9a1-020d93701ed2"
+}
 
-variable "os_external_network" {}
-
-variable "os_tenant_name" {}
-
-variable "os_default_network" {}
-
-variable "os_tenant_id" {}
-
-variable "os_project_name" {}
-
-variable "os_flavor_id" {}
-
-variable "os_login_flavor_id" {}
-
-variable "os_image_id" {}
-
-variable "os_auth_url" {}
-
-variable "os_region_name" {}
+variable "os_image_id" {
+  # CentOS 7
+  default = "5088c906-1636-4319-9dcb-76ab92257731"
+}
