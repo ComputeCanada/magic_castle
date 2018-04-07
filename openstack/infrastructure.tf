344f18ad005c92d88fa1fe3570f78ac9c2ae71fa
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-04-07 11:37:42 -0400

    Move data in their own file

--- openstack/infrastructure.tf
+++ openstack/infrastructure.tf
@@ -7,44 +7,6 @@ provider "openstack" {
   region      = "${var.os_region_name}"
 }
 
-data "template_file" "common" {
-  template = "${file("common.yaml")}"
-
-  vars {
-    nb_nodes      = "${var.nb_nodes}"
-    compute_vcpus = "${var.compute_vcpus}"
-    compute_ram   = "${floor(var.compute_ram * 0.925)}"
-    compute_disk  = "${var.compute_disk}"
-    cluster_name  = "${var.cluster_name}"
-  }
-}
-
-data "template_file" "mgmt" {
-  template = "${file("mgmt.yaml")}"
-
-  vars {
-    admin_passwd = "${var.admin_passwd}"
-    guest_passwd = "${var.guest_passwd}"
-    nb_users     = "${var.nb_users}"
-  }
-}
-
-data "template_cloudinit_config" "mgmt_config" {
-  part {
-    filename     = "common.yaml"
-    merge_type   = "list(append)+dict(recurse_array)+str()"
-    content_type = "text/cloud-config"
-    content      = "${data.template_file.common.rendered}"
-  }
-
-  part {
-    filename     = "mgmt.yaml"
-    merge_type   = "list(append)+dict(recurse_array)+str()"
-    content_type = "text/cloud-config"
-    content      = "${data.template_file.mgmt.rendered}"
-  }
-}
-
 resource "openstack_compute_instance_v2" "mgmt01" {
   name            = "mgmt01"
   flavor_id       = "${var.os_mgmt_flavor_id}"
@@ -66,31 +28,6 @@ resource "openstack_compute_instance_v2" "mgmt01" {
   }
 }
 
-data "template_file" "login" {
-  template = "${file("login.yaml")}"
-
-  vars {
-    admin_passwd = "${var.admin_passwd}"
-    mgmt01_ip    = "${openstack_compute_instance_v2.mgmt01.network.0.fixed_ip_v4}"
-  }
-}
-
-data "template_cloudinit_config" "login_config" {
-  part {
-    filename     = "common.yaml"
-    merge_type   = "list(append)+dict(recurse_array)+str()"
-    content_type = "text/cloud-config"
-    content      = "${data.template_file.common.rendered}"
-  }
-
-  part {
-    filename     = "login.yaml"
-    merge_type   = "list(append)+dict(recurse_array)+str()"
-    content_type = "text/cloud-config"
-    content      = "${data.template_file.login.rendered}"
-  }
-}
-
 resource "openstack_compute_instance_v2" "login01" {
   name     = "${var.cluster_name}01"
   image_id = "${var.os_image_id}"
@@ -102,37 +39,6 @@ resource "openstack_compute_instance_v2" "login01" {
 
   network {
     name = "${var.os_default_network}"
-  }
-}
-
-data "template_file" "node" {
-  template = "${file("node.yaml")}"
-
-  vars {
-    admin_passwd = "${var.admin_passwd}"
-    mgmt01_ip    = "${openstack_compute_instance_v2.mgmt01.network.0.fixed_ip_v4}"
-  }
-}
-
-data "openstack_compute_flavor_v2" "node" {
-  vcpus = "${var.compute_vcpus}"
-  ram   = "${var.compute_ram}"
-  disk  = "${var.compute_disk}"
-}
-
-data "template_cloudinit_config" "node_config" {
-  part {
-    filename     = "common.yaml"
-    merge_type   = "list(append)+dict(recurse_array)+str()"
-    content_type = "text/cloud-config"
-    content      = "${data.template_file.common.rendered}"
-  }
-
-  part {
-    filename     = "node.yaml"
-    merge_type   = "list(append)+dict(recurse_array)+str()"
-    content_type = "text/cloud-config"
-    content      = "${data.template_file.node.rendered}"
   }
 }
 
