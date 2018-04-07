94b3e90d67bba834ced722d7c8a72d429aa722e0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-04-07 13:01:02 -0400

    Move openstack specific variables in their own file

---
+++ openstack/openstack.tf
@@ -0,0 +1,28 @@
+variable "os_ssh_key" {
+  default = "fafor10"
+}
+
+variable "os_external_network" {
+  default = "VLAN3337"
+}
+
+variable "os_default_network" {
+  default = "ipm-500_network"
+}
+
+variable "os_login_flavor_id" {
+  default = "2ff7463c-dda9-4687-8b7a-80ad3303fd41"
+}
+
+variable "os_mgmt_flavor_id" {
+  # p2-3gb
+  default = "2ff7463c-dda9-4687-8b7a-80ad3303fd41"
+
+  # c4-15gb-31
+  # default = "9493fdd3-3100-440d-a9a1-020d93701ed2"
+}
+
+variable "os_image_id" {
+  # CentOS 7
+  default = "5088c906-1636-4319-9dcb-76ab92257731"
+}
--- openstack/variables.tf
+++ openstack/variables.tf
@@ -33,20 +33,3 @@ variable "compute_ram" {
 variable "compute_disk" {
   default = 0
 }
-
-variable "os_login_flavor_id" {
-  default = "2ff7463c-dda9-4687-8b7a-80ad3303fd41"
-}
-
-variable "os_mgmt_flavor_id" {
-  # p2-3gb
-  default = "2ff7463c-dda9-4687-8b7a-80ad3303fd41"
-
-  # c4-15gb-31
-  # default = "9493fdd3-3100-440d-a9a1-020d93701ed2"
-}
-
-variable "os_image_id" {
-  # CentOS 7
-  default = "5088c906-1636-4319-9dcb-76ab92257731"
-}
