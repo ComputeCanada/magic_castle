a3aabcddb38a249a69e5ed8431c4ffef0b307be8
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-05-23 15:33:46 -0400

    Add example for east and west cloud

---
+++ examples/openstack/westcloud/main.tf
@@ -0,0 +1,17 @@
+module "openstack" {
+  source = "git::ssh://gitlab@git.computecanada.ca/fafor10/slurm_cloud.git//openstack"
+
+  # JupyterHub + Slurm definition
+  cluster_name        = "phoenix"
+  nb_nodes            = 5
+  nb_users            = 10
+  shared_storage_size = 100
+  domain_name         = "jupyter2.calculquebec.cloud"
+
+  # OpenStack specifics
+  os_external_network = "VLAN3337"
+  os_image_id         = "5088c906-1636-4319-9dcb-76ab92257731"
+  os_flavor_node      = "p2-3gb"
+  os_flavor_login     = "p2-3gb"
+  os_flavor_mgmt      = "p2-3gb"
+}
