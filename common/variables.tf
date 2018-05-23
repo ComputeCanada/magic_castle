cd7678abbc6fd0cb1bc3cf2cc4f1e192c350cce6
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-05-23 15:31:54 -0400

    Remove default value for all variables in OpenStack

--- common/variables.tf
+++ common/variables.tf
@@ -1,23 +1,11 @@
-variable "cluster_name" {
-  default = "phoenix"
-}
+variable "cluster_name" {}
 
-variable "nb_nodes" {
-  default = 2
-}
+variable "nb_nodes" {}
 
-variable "nb_users" {
-  default = 5
-}
+variable "nb_users" {}
 
-variable "shared_storage_size" {
-  default = 20
-}
+variable "shared_storage_size" {}
 
-variable "public_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtK+7I+alsG4rJvg96ycHCl9Cjugww4D+UnPXCm1qi+kwq/SHyc3dXdPt1ls47jerAj3jfWx39zFovN3eyRsxG0PyMLyl2xpeYxT9BKWVqClijYKfUBj74fFsOj9ma7rw6Y7ksNXA4zL1VVhRH7vIhgjWZZO1VH1f6GYrB6sVBsjodKYQLAF+TLPJsONYOOQe8iKxdOCob/9D5nWRgIARTNGWc4m2EQAciDoZ2Qmoy0BSFs7amhMqytmFk80Ww83K2Fa4lqn/27rMb4NZlbzYKWfmnPXefzW/oa85WmFM+al95Lwg55kXWWTgOCBj1atYizLCQwZ1KSVPcn6fQVB3Yw== felix@AtomicCaptain.local"
-}
+variable "public_key" {}
 
-variable "domain_name" {
-  default = "jupyter2.calculquebec.cloud"
-}
+variable "domain_name" {}
--- openstack/openstack.tf
+++ openstack/openstack.tf
@@ -2,11 +2,6 @@ variable "os_flavor_node" {}
 variable "os_flavor_login" {}
 variable "os_flavor_mgmt" {}
 
-variable "os_external_network" {
-  default = "VLAN3337"
-}
+variable "os_external_network" {}
 
-variable "os_image_id" {
-  # CentOS 7
-  default = "5088c906-1636-4319-9dcb-76ab92257731"
-}
+variable "os_image_id" {}
