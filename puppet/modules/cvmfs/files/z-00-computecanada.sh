d7866c13df1e37a89ea79fcd7eddbc9d5caa5b3e
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-10-02 14:44:45 -0400

    Remove puppet since it now has its own git repo

--- puppet/modules/cvmfs/files/z-00-computecanada.sh
+++
@@ -1,5 +0,0 @@
-if [[ $UID -gt 10000 ]]; then
-    if [[ -r /cvmfs/soft.computecanada.ca/config/profile/bash.sh ]]; then
-        source /cvmfs/soft.computecanada.ca/config/profile/bash.sh
-    fi
-fi
