5608a2e675bb3293ca69f08619ce7ddc4ef7f375
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-11-05 10:03:28 -0500

    Add ecdsa sshfp record

--- common/dns.tf
+++ common/dns.tf
@@ -28,3 +28,24 @@ resource "cloudflare_record" "login_sshfp_rsa_sha256" {
     fingerprint = "${sha256(base64decode(element(split(" ", tls_private_key.login_rsa.public_key_openssh), 1)))}"
   }
 }
+
+resource "cloudflare_record" "login_sshfp_ecdsa_sha1" {
+  domain = "${join(".", slice(split(".", var.domain_name), 1, length(split(".", var.domain_name))))}"
+  name   = "${element(split(".", var.domain_name), 0)}"
+  type   = "SSHFP"
+  data   = {
+    algorithm   = 3
+    type        = 1
+    fingerprint = "${sha1(base64decode(element(split(" ", tls_private_key.login_ecdsa.public_key_openssh), 1)))}"
+  }
+}
+
+resource "cloudflare_record" "login_sshfp_ecdsa_sha256" {
+  domain = "${join(".", slice(split(".", var.domain_name), 1, length(split(".", var.domain_name))))}"
+  name   = "${element(split(".", var.domain_name), 0)}"
+  type   = "SSHFP"
+  data   = {
+    algorithm   = 3
+    type        = 2
+    fingerprint = "${sha256(base64decode(element(split(" ", tls_private_key.login_ecdsa.public_key_openssh), 1)))}"
+  }
