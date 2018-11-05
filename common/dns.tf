79d245bfc6dd92babfe1ba5f43459c037b737f3c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-11-05 09:34:44 -0500

    Add missing domain to SSHFP records

--- common/dns.tf
+++ common/dns.tf
@@ -8,9 +8,10 @@ resource "cloudflare_record" "jupyter" {
 }
 
 resource "cloudflare_record" "login_sshfp_rsa_sha1" {
-  name  = "${element(split(".", var.domain_name), 0)}"
-  type  = "SSHFP"
-  data  = {
+  domain = "${join(".", slice(split(".", var.domain_name), 1, length(split(".", var.domain_name))))}"
+  name   = "${element(split(".", var.domain_name), 0)}"
+  type   = "SSHFP"
+  data   = {
     algorithm   = 1
     type        = 1
     fingerprint = "${sha1(base64decode(element(split(" ", tls_private_key.login_rsa.public_key_openssh), 1)))}"
@@ -18,9 +19,10 @@ resource "cloudflare_record" "login_sshfp_rsa_sha1" {
 }
 
 resource "cloudflare_record" "login_sshfp_rsa_sha256" {
-  name  = "${element(split(".", var.domain_name), 0)}"
-  type  = "SSHFP"
-  data  = {
+  domain = "${join(".", slice(split(".", var.domain_name), 1, length(split(".", var.domain_name))))}"
+  name   = "${element(split(".", var.domain_name), 0)}"
+  type   = "SSHFP"
+  data   = {
     algorithm   = 1
     type        = 2
     fingerprint = "${sha256(base64decode(element(split(" ", tls_private_key.login_rsa.public_key_openssh), 1)))}"
