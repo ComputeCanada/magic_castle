81dca2d4ddad291be7d6f4940b0f444104a00df8
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2018-11-05 09:32:35 -0500

    Add SSHFP records for RSA to dns.tf

--- common/dns.tf
+++ common/dns.tf
@@ -6,3 +6,23 @@ resource "cloudflare_record" "jupyter" {
   value  = "${local.public_ip}"
   type   = "A"
 }
+
+resource "cloudflare_record" "login_sshfp_rsa_sha1" {
+  name  = "${element(split(".", var.domain_name), 0)}"
+  type  = "SSHFP"
+  data  = {
+    algorithm   = 1
+    type        = 1
+    fingerprint = "${sha1(base64decode(element(split(" ", tls_private_key.login_rsa.public_key_openssh), 1)))}"
+  }
+}
+
+resource "cloudflare_record" "login_sshfp_rsa_sha256" {
+  name  = "${element(split(".", var.domain_name), 0)}"
+  type  = "SSHFP"
+  data  = {
+    algorithm   = 1
+    type        = 2
+    fingerprint = "${sha256(base64decode(element(split(" ", tls_private_key.login_rsa.public_key_openssh), 1)))}"
+  }
+}
