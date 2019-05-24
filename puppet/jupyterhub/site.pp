26e8cb6756f0b9e320732af5529408ac7adef489
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-05-24 09:49:01 -0400

    Define site for login and mgmt > 01

--- puppet/jupyterhub/site.pp
+++ puppet/jupyterhub/site.pp
@@ -1,8 +1,6 @@
 node default {
   include profile::base
   include profile::freeipa::client
-  include profile::nfs::client
-  include profile::cvmfs::client
   include profile::rsyslog::client
 }
 
@@ -19,6 +17,15 @@ node /^login01$/ {
   include profile::fail2ban
 }
 
+node /^login0*(?:[2-9]|[1-9]\d\d*)$/ {
+  include profile::base
+  include profile::freeipa::client
+  include profile::nfs::client
+  include profile::cvmfs::client
+  include profile::rsyslog::client
+  include profile::slurm::submitter
+}
+
 node /^mgmt01$/ {
   include profile::base
   include profile::freeipa::server
@@ -29,6 +36,13 @@ node /^mgmt01$/ {
   include profile::freeipa::guest_accounts
   include profile::slurm::accounting
   include profile::squid::server
+}
+
+node /^mgmt0*(?:[2-9]|[1-9]\d\d*)$/ {
+  include profile::base
+  include profile::freeipa::client
+  include profile::cvmfs::client
+  include profile::rsyslog::client
 }
 
 node /^node\d+$/ {
