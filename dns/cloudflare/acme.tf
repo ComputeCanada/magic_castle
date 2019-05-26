6c1713a584ea787d49beb9224faa94f8e3c4bf7c
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-05-26 00:13:51 -0400

    Add ability to generate ssl certificate with dns

---
+++ dns/cloudflare/acme.tf
@@ -0,0 +1,48 @@
+provider "acme" {
+  server_url = "https://acme-v02.api.letsencrypt.org/directory"
+}
+
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+}
+
+resource "acme_registration" "reg" {
+  account_key_pem = "${tls_private_key.private_key.private_key_pem}"
+  email_address   = "felix-antoine.fortin@calculquebec.ca"
+}
+
+resource "acme_certificate" "certificate" {
+  account_key_pem           = "${acme_registration.reg.account_key_pem}"
+  common_name               = "${var.name}.${var.domain}"
+
+  dns_challenge {
+    provider = "cloudflare"
+  }
+}
+
+resource "null_resource" "deploy_certs" {
+  count = "${var.nb_login}"
+
+  connection {
+      type     = "ssh"
+      user     = "centos"
+      host     = "${element(var.public_ip, count.index)}"
+  }
+
+  provisioner "file" {
+    content     = "${acme_certificate.certificate.private_key_pem}"
+    destination = "/home/centos/privkey.pem"
+  }
+
+  provisioner "file" {
+    content     = "${acme_certificate.certificate.certificate_pem}${acme_certificate.certificate.issuer_pem}"
+    destination = "/home/centos/fullchain.pem"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo cp /home/centos/fullchain.pem /home/centos/privkey.pem /root",
+      "rm fullchain.pem privkey.pem",
+    ]
+  }
+}
