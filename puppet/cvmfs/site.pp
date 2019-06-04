27c1fd0a584561ffcfe43871c8e026c151ceb6e9
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-06-04 17:22:41 -0400

    Reorder include in site.pp to accelerate provisioning

--- puppet/cvmfs/site.pp
+++ puppet/cvmfs/site.pp
@@ -1,48 +1,48 @@
 node default {
   include profile::base
-  include profile::freeipa::client
   include profile::rsyslog::client
+  include profile::freeipa::client
 }
 
 node /^login\d+$/ {
   include profile::base
-  include profile::freeipa::client
-  include profile::nfs::client
+  include profile::fail2ban
   include profile::cvmfs::client
   include profile::rsyslog::client
   include profile::slurm::submitter
   include profile::singularity
-  include profile::fail2ban
+  include profile::freeipa::client
+  include profile::nfs::client
 }
 
 node /^mgmt01$/ {
-  include profile::base
-  include profile::freeipa::server
   include profile::rsyslog::server
+  include profile::freeipa::server
+  include profile::squid::server
   include profile::slurm::controller
   include profile::nfs::server
-
+  
+  include profile::base
   include profile::freeipa::guest_accounts
   include profile::slurm::accounting
-  include profile::squid::server
 }
 
 node /^mgmt0*(?:[2-9]|[1-9]\d\d*)$/ {
   include profile::base
-  include profile::freeipa::client
   include profile::rsyslog::client
+  include profile::freeipa::client
 }
 
 node /^node\d+$/ {
   include profile::base
-  include profile::freeipa::client
-  include profile::nfs::client
   include profile::rsyslog::client
   include profile::cvmfs::client
   include profile::gpu
-  include profile::slurm::node
   include profile::singularity
 
+  include profile::nfs::client
+  include profile::freeipa::client
+  include profile::slurm::node
   Class['profile::freeipa::client'] -> Class['profile::slurm::node']
   Class['profile::nfs::client'] -> Class['profile::slurm::node']
 }
--- puppet/jupyterhub/site.pp
+++ puppet/jupyterhub/site.pp
@@ -1,51 +1,51 @@
 node default {
   include profile::base
-  include profile::freeipa::client
   include profile::rsyslog::client
+  include profile::freeipa::client
 }
 
 node /^login\d+$/ {
   include profile::base
-  include profile::freeipa::client
-  include profile::nfs::client
+  include profile::fail2ban
   include profile::cvmfs::client
   include profile::rsyslog::client
   include profile::slurm::submitter
   include profile::globus::base
   include profile::singularity
   include jupyterhub
-  include profile::fail2ban
+  include profile::freeipa::client
+  include profile::nfs::client
 }
 
 node /^mgmt01$/ {
-  include profile::base
-  include profile::freeipa::server
   include profile::rsyslog::server
-  include profile::slurm::controller
+  include profile::freeipa::server
+  include profile::squid::server
   include profile::nfs::server
+  include profile::slurm::controller
 
+  include profile::base
   include profile::freeipa::guest_accounts
   include profile::slurm::accounting
-  include profile::squid::server
 }
 
 node /^mgmt0*(?:[2-9]|[1-9]\d\d*)$/ {
   include profile::base
-  include profile::freeipa::client
   include profile::rsyslog::client
+  include profile::freeipa::client
 }
 
 node /^node\d+$/ {
   include profile::base
-  include profile::freeipa::client
-  include profile::nfs::client
   include profile::rsyslog::client
   include profile::cvmfs::client
   include profile::gpu
-  include profile::slurm::node
   include profile::singularity
   include jupyterhub::node
 
+  include profile::nfs::client
+  include profile::freeipa::client
+  include profile::slurm::node
   Class['profile::freeipa::client'] -> Class['profile::slurm::node']
   Class['profile::nfs::client'] -> Class['profile::slurm::node']
 }
