7a8ba8cda324c3490afbfa8218b4b8deb67ca4a2
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-06-27 15:18:49 -0400

    Upgrade azure example to terraform 0.12

--- examples/azure/main.tf
+++ examples/azure/main.tf
@@ -2,31 +2,31 @@ module "azure" {
   source = "git::ssh://gitlab@git.computecanada.ca/magic_castle/slurm_cloud.git//azure"
 
   # Cluster customization
-  puppet_config       = "jupyterhub"
-  cluster_name        = "phoenix"
-  domain              = "calculquebec.cloud"
-  nb_nodes            = 5
-  nb_users            = 10
-  home_size           = 100
-  project_size        = 50
-  scratch_size        = 50
+  puppet_config   = "jupyterhub"
+  cluster_name    = "phoenix"
+  domain          = "calculquebec.cloud"
+  nb_nodes        = 5
+  nb_users        = 10
+  home_size       = 100
+  project_size    = 50
+  scratch_size    = 50
   public_key_path = "./key.pub"
 
   # Azure specifics
-  location            = "eastus"
-  vm_size_mgmt        = "Standard_DS1_v2"
-  vm_size_login       = "Standard_DS1_v2"
-  vm_size_node        = "Standard_DS1_v2"
+  location      = "eastus"
+  vm_size_mgmt  = "Standard_DS1_v2"
+  vm_size_login = "Standard_DS1_v2"
+  vm_size_node  = "Standard_DS1_v2"
 }
 
 output "public_ip" {
-	value = "${module.azure.ip}"
+  value = module.azure.ip
 }
 
 output "domain_name" {
-	value = "${module.azure.domain_name}"
+  value = module.azure.domain_name
 }
 
 output "guest_passwd" {
-	value = "${module.azure.guest_passwd}"
+  value = module.azure.guest_passwd
 }
