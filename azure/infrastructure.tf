dcf18122a8c3f90847a2e86e345916bf45c1fc5b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-06-28 15:52:43 -0400

    Define azure firewall rules based on var.firewall_rules

--- azure/infrastructure.tf
+++ azure/infrastructure.tf
@@ -59,40 +59,20 @@ resource "azurerm_network_security_group" "security_login" {
   location            = var.location
   resource_group_name = azurerm_resource_group.group.name
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTPS"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  dynamic "security_rule" {
+    for_each = var.firewall_rules
+    iterator = rule
+    content {
+      name                       = rule.value.name
+      priority                   = (100 + rule.value.from_port) % 4096
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = title(rule.value.ip_protocol)
+      source_port_range          = "*"
+      destination_port_range     = "${rule.value.from_port}-${rule.value.to_port}"
+      source_address_prefix      = "*"
+      destination_address_prefix = rule.value.cidr
+    }
   }
 }
 
