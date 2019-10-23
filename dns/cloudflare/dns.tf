a2bf20faadde427c1edcc573d8d2362aee673c58
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-10-23 14:26:20 -0400

    Update cloudflare dns api to 2.0
    Also fix the version since 2.0 broke all previous version of MC.

--- dns/cloudflare/dns.tf
+++ dns/cloudflare/dns.tf
@@ -1,10 +1,19 @@
 provider "cloudflare" {
+  version = "~> 2.0"
 }
 
 variable "name" {
 }
 
 variable "domain" {
+}
+
+data "cloudflare_zones" "domain" {
+  filter {
+    name   = var.domain
+    status = "active"
+    paused = false
+  }
 }
 
 variable "public_ip" {
@@ -22,19 +31,19 @@ data "external" "key2fp" {
 }
 
 resource "cloudflare_record" "loginX_A" {
-  count  = length(var.public_ip)
-  domain = var.domain
-  name   = join("", [var.name, format("%d", count.index + 1)])
-  value  = var.public_ip[count.index]
-  type   = "A"
+  count   = length(var.public_ip)
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = join("", [var.name, format("%d", count.index + 1)])
+  value   = var.public_ip[count.index]
+  type    = "A"
 }
 
 resource "cloudflare_record" "loginX_sshfp_rsa_sha1" {
-  count  = length(var.public_ip)
-  domain = var.domain
-  name   = join("", [var.name, format("%d", count.index + 1)])
-  type   = "SSHFP"
-  data = {
+  count   = length(var.public_ip)
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = join("", [var.name, format("%d", count.index + 1)])
+  type    = "SSHFP"
+  data    = {
     algorithm   = data.external.key2fp.result["algorithm"]
     type        = 1
     fingerprint = data.external.key2fp.result["sha1"]
@@ -42,11 +51,11 @@ resource "cloudflare_record" "loginX_sshfp_rsa_sha1" {
 }
 
 resource "cloudflare_record" "loginX_sshfp_rsa_sha256" {
-  count  = length(var.public_ip)
-  domain = var.domain
-  name   = join("", [var.name, format("%d", count.index + 1)])
-  type   = "SSHFP"
-  data = {
+  count   = length(var.public_ip)
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = join("", [var.name, format("%d", count.index + 1)])
+  type    = "SSHFP"
+  data    = {
     algorithm   = data.external.key2fp.result["algorithm"]
     type        = 2
     fingerprint = data.external.key2fp.result["sha256"]
@@ -54,26 +63,25 @@ resource "cloudflare_record" "loginX_sshfp_rsa_sha256" {
 }
 
 resource "cloudflare_record" "login_A" {
-  count  = max(length(var.public_ip), 1)
-  domain = var.domain
-  name   = var.name
-  value  = var.public_ip[count.index]
-  type   = "A"
+  count   = max(length(var.public_ip), 1)
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = var.name
+  value   = var.public_ip[count.index]
+  type    = "A"
 }
 
 resource "cloudflare_record" "jupyter_A" {
-  domain = var.domain
-  name   = "jupyter.${var.name}"
-
-  value = var.public_ip[0]
-  type  = "A"
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = "jupyter.${var.name}"
+  value   = var.public_ip[0]
+  type    = "A"
 }
 
 resource "cloudflare_record" "login_sshfp_rsa_sha1" {
-  domain = var.domain
-  name   = var.name
-  type   = "SSHFP"
-  data = {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = var.name
+  type    = "SSHFP"
+  data    = {
     algorithm   = data.external.key2fp.result["algorithm"]
     type        = 1
     fingerprint = data.external.key2fp.result["sha1"]
@@ -81,10 +89,10 @@ resource "cloudflare_record" "login_sshfp_rsa_sha1" {
 }
 
 resource "cloudflare_record" "login_sshfp_rsa_sha256" {
-  domain = var.domain
-  name   = var.name
-  type   = "SSHFP"
-  data = {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = var.name
+  type    = "SSHFP"
+  data    = {
     algorithm   = data.external.key2fp.result["algorithm"]
     type        = 2
     fingerprint = data.external.key2fp.result["sha256"]
