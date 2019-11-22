b8073684c49f71ea994fc5e8b718141a1d8797fb
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-11-22 15:41:17 -0500

    Add release script for BSD platform

---
+++ release.bsd.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+VERSION=$1
+
+if [ -z "$VERSION" ]
+then
+    echo "Specify the release version number"
+    echo "Usage: release.sh VERSION"
+    exit 1
+fi
+
+TMPDIR=$(mktemp -d)
+FOLDER=$TMPDIR/magic_castle-$VERSION
+#CLOUD=(aws azure gcp openstack ovh)
+CLOUD=(openstack)
+
+mkdir -p releases
+for provider in "${CLOUD[@]}"; do
+    cur_folder=$FOLDER/magic_castle-$provider-$VERSION
+    mkdir -p $cur_folder
+    mkdir -p $cur_folder/$provider/cloud-init/
+    cp -RfL $provider/*.tf $cur_folder/$provider/
+    #cp -RfL $provider/*.sh $cur_folder/$provider/
+    cp -RfL cloud-init/*.yaml $cur_folder/$provider/cloud-init/
+    cp -RfL dns $cur_folder
+    cp -fL examples/$provider/main.tf $cur_folder
+    sed -i "" 's;git::https://github.com/ComputeCanada/magic_castle.git//;./;g' $cur_folder/main.tf
+    sed -i "" "s;default = \"master\";default = \"$VERSION\";" $cur_folder/$provider/variables.tf
+    cp LICENSE $cur_folder
+    cp $provider/README.md $cur_folder
+
+    # Identify and fix provider versions
+    TF_PROVIDERS=$(terraform init $cur_folder | grep '* provider' | sed -E 's/\* provider\.([a-z]*): version = "~> ([0-9.]*)"/\1_\2/g')
+    for prov_vers in $TF_PROVIDERS; do
+        prov="${prov_vers%%_*}"
+        vers="${prov_vers#*_}"
+        file=$(grep -l -R "provider \"$prov\"" $cur_folder)
+        if [ ! -z "$file" ]; then
+            sed -i "" "/provider \"$prov\"/  a\ 
+\ \ version = \"~> ${vers}\"
+" $file
+            echo "sed version = ~> ${vers}"
+        else
+            echo "provider \"${prov}\" { version = \"~> ${vers}\" } " >> $cur_folder/$provider/providers.tf
+        fi
+    done
+
+    cd $FOLDER
+    tar czvf magic_castle-$provider-$VERSION.tar.gz magic_castle-$provider-$VERSION 
+    zip magic_castle-$provider-$VERSION.zip -r magic_castle-$provider-$VERSION
+    cd -
+    cp $FOLDER/magic_castle-$provider-$VERSION.tar.gz releases/
+    cp $FOLDER/magic_castle-$provider-$VERSION.zip releases/
+done
+rm -rf $TMPDIR
