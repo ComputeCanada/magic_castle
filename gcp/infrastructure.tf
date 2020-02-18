ComputeCanada/magic_castle

commit 78acb54c6964e519dfa56b55e695e0e28c0112df

Add prefix to the name of volumes and ipv4 in GCP

gcp/infrastructure.tf
@@ -34,7 +34,7 @@ resource "google_compute_router_nat" "nat" {
 
 resource "google_compute_disk" "home" {
   count = lower(var.storage["type"]) == "nfs" ? 1 : 0
-  name  = "home"
+  name  = "${var.cluster_name}-home"
   type  = "pd-standard"
   zone  = var.zone
   size  = var.storage["home_size"]
@@ -42,7 +42,7 @@ resource "google_compute_disk" "home" {
 
 resource "google_compute_disk" "project" {
   count = lower(var.storage["type"]) == "nfs" ? 1 : 0
-  name  = "project"
+  name  = "${var.cluster_name}-project"
   type  = "pd-standard"
   zone  = var.zone
   size  = var.storage["project_size"]
@@ -50,7 +50,7 @@ resource "google_compute_disk" "project" {
 
 resource "google_compute_disk" "scratch" {
   count = lower(var.storage["type"]) == "nfs" ? 1 : 0
-  name  = "scratch"
+  name  = "${var.cluster_name}-scratch"
   type  = "pd-standard"
   zone  = var.zone
   size  = var.storage["scratch_size"]
@@ -128,7 +128,7 @@ resource "google_compute_attached_disk" "scratch" {
 
 resource "google_compute_address" "static" {
   count = max(var.instances["login"]["count"], 1)
-  name  = format("login%d-ipv4", count.index + 1)
+  name  = format("%s-login%d-ipv4",  var.cluster_name, count.index + 1)
 }
 
 resource "google_compute_instance" "login" {

commit 5361a240df9a1eb6960d8305ac5c0e80988369aa

Fix regression introduced in 78acb54
Adding the cluster_name as a prefix to the volume names broke the
device names in locals. We will now use the resource name from now
on and avoid breaking the mounts in the future.

gcp/infrastructure.tf
@@ -251,7 +251,7 @@ resource "google_compute_firewall" "default" {
 locals {
   mgmt1_ip   = google_compute_address.mgmt[0].address
   public_ip   = google_compute_address.static[*].address
-  home_dev    = "/dev/disk/by-id/google-home"
-  project_dev = "/dev/disk/by-id/google-project"
-  scratch_dev = "/dev/disk/by-id/google-scratch"
+  home_dev    = lower(var.storage["type"]) == "nfs" ? "/dev/disk/by-id/google-${google_compute_disk.home[0].name}" : ""
+  project_dev = lower(var.storage["type"]) == "nfs" ? "/dev/disk/by-id/google-${google_compute_disk.project[0].name}" : ""
+  scratch_dev = lower(var.storage["type"]) == "nfs" ? "/dev/disk/by-id/google-${google_compute_disk.scratch[0].name}" : ""
 }