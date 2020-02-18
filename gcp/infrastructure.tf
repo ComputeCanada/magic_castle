ComputeCanada/magic_castle

commit 6b9cbc9813b65407a4454cefa8a84d6e820ced16

Make changes to image variable ignored after cluster is built

gcp/infrastructure.tf
@@ -95,7 +95,10 @@ resource "google_compute_instance" "mgmt" {
   metadata_startup_script = file("${path.module}/install_cloudinit.sh")
 
   lifecycle {
-    ignore_changes = [attached_disk]
+    ignore_changes = [
+      attached_disk,
+      boot_disk.initialize_params.image
+    ]
   }
 }
 
@@ -157,6 +160,12 @@ resource "google_compute_instance" "login" {
   }
 
   metadata_startup_script = file("${path.module}/install_cloudinit.sh")
+
+  lifecycle {
+    ignore_changes = [
+      boot_disk.initialize_params.image
+    ]
+  }
 }
 
 resource "google_compute_instance" "node" {
@@ -193,6 +202,12 @@ resource "google_compute_instance" "node" {
   }
 
   metadata_startup_script = file("${path.module}/install_cloudinit.sh")
+
+  lifecycle {
+    ignore_changes = [
+      boot_disk.initialize_params.image
+    ]
+  }
 }
 
 resource "google_compute_firewall" "allow_all_internal" {

aws/infrastructure.tf
@@ -156,6 +156,12 @@ resource "aws_instance" "mgmt" {
     Name = format("mgmt%d", count.index + 1)
   }
 
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
+
   depends_on = [aws_internet_gateway.gw]
 }
 
@@ -244,6 +250,12 @@ resource "aws_instance" "login" {
     aws_security_group.allow_out_any.id,
   ]
 
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
+
   tags = {
     Name = format("login%d", count.index + 1)
   }
@@ -278,6 +290,12 @@ resource "aws_instance" "node" {
     aws_security_group.allow_any_inside_vpc.id,
     aws_security_group.allow_out_any.id,
   ]
+
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
 
   tags = {
     Name = "node${count.index + 1}"

azure/infrastructure.tf
@@ -168,6 +168,12 @@ resource "azurerm_virtual_machine" "login" {
   os_profile_linux_config {
     disable_password_authentication = true
   }
+
+  lifecycle {
+    ignore_changes = [
+      storage_image_reference
+    ]
+  }
 }
 
 resource "azurerm_virtual_machine" "mgmt" {
@@ -201,6 +207,12 @@ resource "azurerm_virtual_machine" "mgmt" {
 
   os_profile_linux_config {
     disable_password_authentication = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      storage_image_reference
+    ]
   }
 }
 
@@ -289,6 +301,12 @@ resource "azurerm_virtual_machine" "nodevm" {
 
   os_profile_linux_config {
     disable_password_authentication = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      storage_image_reference
+    ]
   }
 }
 

openstack/infrastructure.tf
@@ -120,6 +120,13 @@ resource "openstack_compute_instance_v2" "mgmt" {
       volume_size           = block_device.value.volume_size
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      image_id,
+      block_device[0].uuid
+    ]
+  }
 }
 
 resource "openstack_compute_volume_attach_v2" "va_home" {
@@ -182,6 +189,13 @@ resource "openstack_compute_instance_v2" "login" {
       volume_size           = block_device.value.volume_size
     }
   }
+
+  lifecycle {
+    ignore_changes = [
+      image_id,
+      block_device[0].uuid
+    ]
+  }
 }
 
 resource "openstack_networking_port_v2" "port_node" {
@@ -225,6 +239,13 @@ resource "openstack_compute_instance_v2" "node" {
       delete_on_termination = true
       volume_size           = block_device.value.volume_size
     }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      image_id,
+      block_device[0].uuid
+    ]
   }
 }
 

commit c7b4e5380691e6981a1ec944fe217a658bfdb188

Fix regression introduced in 6b9cbc9
boot_disk and initialize_params are lists

gcp/infrastructure.tf
@@ -97,7 +97,7 @@ resource "google_compute_instance" "mgmt" {
   lifecycle {
     ignore_changes = [
       attached_disk,
-      boot_disk.initialize_params.image
+      boot_disk[0].initialize_params[0].image
     ]
   }
 }
@@ -163,7 +163,7 @@ resource "google_compute_instance" "login" {
 
   lifecycle {
     ignore_changes = [
-      boot_disk.initialize_params.image
+      boot_disk[0].initialize_params[0].image
     ]
   }
 }
@@ -205,7 +205,7 @@ resource "google_compute_instance" "node" {
 
   lifecycle {
     ignore_changes = [
-      boot_disk.initialize_params.image
+      boot_disk[0].initialize_params[0].image
     ]
   }
 }