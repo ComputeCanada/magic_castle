707401e4eb68a28aeb052357db2283116193a991
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-02-24 15:30:09 -0500

    Fix release.bsd.sh

--- release.bsd.sh
+++ release.bsd.sh
@@ -10,8 +10,7 @@ fi
 
 TMPDIR=$(mktemp -d)
 FOLDER=$TMPDIR/magic_castle-$VERSION
-#CLOUD=(aws azure gcp openstack ovh)
-CLOUD=(openstack)
+CLOUD=(aws azure gcp openstack ovh)
 
 mkdir -p releases
 for provider in "${CLOUD[@]}"; do
@@ -26,23 +25,30 @@ for provider in "${CLOUD[@]}"; do
     cp LICENSE $cur_folder
 
     # Identify and fix provider versions
-    TF_PROVIDERS=$(find $cur_folder -type d -exec terraform init {} \; |
-                grep '* provider' |
-                sed -E 's/\* provider\.([a-z]*): version = "~> ([0-9.]*)"/\1_\2/g' |
-                sort |
-                uniq)
+    # Keeping only the lowest version of a provider if it is present more than once
+    TF_PROVIDERS=$(
+        find $cur_folder -type d -exec terraform init {} \; |
+        grep 'Downloading plugin' |
+        sed -E 's/- Downloading plugin for provider "([a-z]*)".*([0-9]{1,}\.[0-9]{1,})\.[0-9]{1,}\.\.\./\2 \1/g' |
+        sort -V |
+        uniq -f 1 |
+        awk '{print $2,$1}' |
+        tr " " "_" |
+        sort
+    )
+
+    echo -e "terraform {\n  required_providers {"  >> $cur_folder/providers.tf
     for prov_vers in $TF_PROVIDERS; do
         prov="${prov_vers%%_*}"
         vers="${prov_vers#*_}"
         file=$(grep -l -R "provider \"$prov\"" $cur_folder)
+        file=$(grep -l -R "provider \"$prov\"" $cur_folder)
         if [ ! -z "$file" ]; then
-            sed -i "" "/provider \"$prov\"/  a\ 
-\ \ version = \"~> ${vers}\"
-" $file
-        else
-            echo "provider \"${prov}\" { version = \"~> ${vers}\" } " >> $cur_folder/$provider/providers.tf
+            sed -i ''  '/version = "[<>~]\{1,\} *[0-9.]*"/d' $file
         fi
+        echo "    ${prov} = \"~> ${vers}\" " >> $cur_folder/providers.tf
     done
+    echo -e "  }\n}"  >> $cur_folder/providers.tf
 
     cd $FOLDER
     tar czvf magic_castle-$provider-$VERSION.tar.gz magic_castle-$provider-$VERSION
