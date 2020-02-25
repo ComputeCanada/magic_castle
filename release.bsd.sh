756b6d2267fe3a5b1e11a7dc9156cf6384298509
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-02-25 09:54:56 -0500

    Add cloud arguments to release.sh and release.bsd.sh
    The arguments allow to build a release for a subset
    of providers.

--- release.bsd.sh
+++ release.bsd.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 VERSION=$1
+shift
+CLOUD=($@)
 
-if [ -z "$VERSION" ]
-then
+if [ -z "$VERSION" ]; then
     echo "Specify the release version number"
     echo "Usage: release.sh VERSION"
     exit 1
 fi
 
+if [ -z "$CLOUD" ]; then
+    CLOUD=(aws azure gcp openstack ovh)
+fi
+
 TMPDIR=$(mktemp -d)
 FOLDER=$TMPDIR/magic_castle-$VERSION
-CLOUD=(aws azure gcp openstack ovh)
 
 mkdir -p releases
 for provider in "${CLOUD[@]}"; do
