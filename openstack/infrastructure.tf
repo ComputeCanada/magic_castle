ComputeCanada/magic_castle

commit c09ea17de1441ad73a2fadc16cbd5768262f265c

Add ability to create compute nodes with different type
Update ovh example for heterogenous node types
Update gcp module for heterogenous node type
Update azure module to allow heterogenous node types
Update aws module to allow heterogenous node types
Add node prefix documentation
Update changelog
Update Terraform minimum version required

openstack/infrastructure.tf
@@ -14,7 +14,8 @@ data "openstack_compute_flavor_v2" "login" {
 }
 
 data "openstack_compute_flavor_v2" "node" {
-  name = var.instances["node"]["type"]
+  for_each = local.node
+  name = each.value.type
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup_1" {
@@ -199,8 +200,8 @@ resource "openstack_compute_instance_v2" "login" {
 }
 
 resource "openstack_networking_port_v2" "port_node" {
-  count              = var.instances["node"]["count"]
-  name               = format("port-node%d", count.index + 1)
+  for_each           = local.node
+  name               = format("port-%s", each.key)
   network_id         = local.network.id
   security_group_ids = [openstack_compute_secgroup_v2.secgroup_1.id]
   fixed_ip {
@@ -208,21 +209,38 @@ resource "openstack_networking_port_v2" "port_node" {
   }
 }
 
-resource "openstack_compute_instance_v2" "node" {
-  count    = var.instances["node"]["count"]
-  name     = "node${count.index + 1}"
-  image_id = var.root_disk_size > data.openstack_compute_flavor_v2.node.disk ? null : data.openstack_images_image_v2.image.id
+locals {
+  node_map = {
+    for key in keys(local.node):
+      key => merge(
+        {
+          image_id  = data.openstack_images_image_v2.image.id,
+          port      = openstack_networking_port_v2.port_node[key].id,
+          networks  = local.ext_networks,
+          root_disk = var.root_disk_size > data.openstack_compute_flavor_v2.node[key].disk ? [{volume_size = var.root_disk_size}] : []
+          user_data = data.template_cloudinit_config.node_config[key].rendered
+        },
+        local.node[key]
+    )
+  }
+}
 
-  flavor_name     = var.instances["node"]["type"]
+resource "openstack_compute_instance_v2" "node" {
+  for_each = local.node_map
+  name     = each.key
+
+  image_id = each.value["root_disk"] == [] ? each.value["image_id"] : null
+
+  flavor_name     = each.value["type"]
   key_pair        = openstack_compute_keypair_v2.keypair.name
   security_groups = [openstack_compute_secgroup_v2.secgroup_1.name]
-  user_data       = data.template_cloudinit_config.node_config[count.index].rendered
+  user_data       = each.value["user_data"]
 
   network {
-    port = openstack_networking_port_v2.port_node[count.index].id
+    port = each.value["port"]
   }
   dynamic "network" {
-    for_each = local.ext_networks
+    for_each = each.value["networks"]
     content {
       access_network = network.value.access_network
       name           = network.value.name
@@ -230,9 +248,9 @@ resource "openstack_compute_instance_v2" "node" {
   }
 
   dynamic "block_device" {
-    for_each = var.root_disk_size > data.openstack_compute_flavor_v2.node.disk ? [{volume_size = var.root_disk_size}] : []
+    for_each = each.value["root_disk"]
     content {
-      uuid                  = data.openstack_images_image_v2.image.id
+      uuid                  = each.value["image_id"]
       source_type           = "image"
       destination_type      = "volume"
       boot_index            = 0

aws/infrastructure.tf
@@ -268,13 +268,25 @@ resource "aws_eip" "login" {
   depends_on = [aws_internet_gateway.gw]
 }
 
+locals {
+  node_map = {
+    for key in keys(local.node):
+      key => merge(
+        {
+          image          = var.image,
+          user_data      = data.template_cloudinit_config.node_config[key].rendered,
+          root_disk_size = var.root_disk_size,
+        },
+        local.node[key]
+    )
+  }
+}
 
 resource "aws_instance" "node" {
-  count         = var.instances["node"]["count"]
-  instance_type = var.instances["node"]["type"]
-  ami           = var.image
-
-  user_data = data.template_cloudinit_config.node_config[count.index].rendered
+  for_each      = local.node_map
+  instance_type = each.value["type"]
+  ami           = each.value["image"]
+  user_data     = each.value["user_data"]
 
   subnet_id                   = aws_subnet.private_subnet.id
   key_name                    = aws_key_pair.key.key_name
@@ -283,7 +295,7 @@ resource "aws_instance" "node" {
   ebs_optimized = true
   root_block_device {
     volume_type = "gp2"
-    volume_size = var.root_disk_size
+    volume_size = each.value["root_disk_size"]
   }
 
   vpc_security_group_ids = [
@@ -298,7 +310,7 @@ resource "aws_instance" "node" {
   }
 
   tags = {
-    Name = "node${count.index + 1}"
+    Name = each.key
   }
 }
 

azure/infrastructure.tf
@@ -1,5 +1,6 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
+  version = "1.44.0"
 }
 
 # Create a resource group
@@ -42,11 +43,11 @@ resource "azurerm_public_ip" "mgmtIP" {
 }
 
 resource "azurerm_public_ip" "nodeIP" {
-  count                        = var.instances["node"]["count"]
-  name                         = format("%s-node-ip-%d", var.cluster_name, count.index + 1)
-  location                     = var.location
-  resource_group_name          = azurerm_resource_group.group.name
-  allocation_method            = "Dynamic"
+  for_each            = local.node
+  name                = format("%s-%s-ip", var.cluster_name, each.key)
+  location            = var.location
+  resource_group_name = azurerm_resource_group.group.name
+  allocation_method   = "Dynamic"
 }
 
 # Create Network Security Group and rule
@@ -122,16 +123,16 @@ resource "azurerm_network_interface" "mgmtNIC" {
 }
 
 resource "azurerm_network_interface" "nodeNIC" {
-  count               = var.instances["node"]["count"]
-  name                = format("%s-node%d-nic", var.cluster_name, count.index + 1)
+  for_each            = local.node
+  name                = format("%s-%s-nic", var.cluster_name, each.key)
   location            = var.location
   resource_group_name = azurerm_resource_group.group.name
 
   ip_configuration {
-    name                          = "${var.cluster_name}_node_nicconfig"
+    name                          = format("%s-%s-ipconfig", var.cluster_name, each.key)
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.nodeIP[count.index].id
+    public_ip_address_id          = azurerm_public_ip.nodeIP[each.key].id
   }
 }
 
@@ -286,39 +287,58 @@ resource "azurerm_virtual_machine_data_disk_attachment" "scratch" {
   caching            = "ReadWrite"
 }
 
-resource "azurerm_virtual_machine" "nodevm" {
-  name                  = format("%s-node%d", var.cluster_name, count.index + 1)
-  count                 = var.instances["node"]["count"]
-  vm_size               = var.instances["node"]["type"]
-  location              = var.location
-  resource_group_name   = azurerm_resource_group.group.name
-  network_interface_ids = [azurerm_network_interface.nodeNIC[count.index].id]
-
-  storage_os_disk {
-    name              = "${var.cluster_name}_node${count.index + 1}_disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = var.managed_disk_type
-    disk_size_gb      = var.root_disk_size
+locals {
+  node_map = {
+    for key in keys(local.node):
+      key => merge(
+        {
+          name              = format("%s-%s", var.cluster_name, key),
+          location          = var.location,
+          image             = var.image,
+          os_disk_name      = format("%s-%s-%s", var.cluster_name, key, "disk")
+          managed_disk_type = var.managed_disk_type
+          root_disk_size    = var.root_disk_size,
+          user_data         = data.template_cloudinit_config.node_config[key].rendered,
+          public_keys       = var.public_keys,
+        },
+        local.node[key]
+    )
   }
+}
+
+resource "azurerm_virtual_machine" "nodevm" {
+  for_each              = local.node_map
+  name                  = each.value["name"]
+  vm_size               = each.value["type"]
+  location              = each.value["location"]
+  resource_group_name   = azurerm_resource_group.group.name
+  network_interface_ids = [azurerm_network_interface.nodeNIC[each.key].id]
 
   storage_image_reference {
-    publisher = var.image["publisher"]
-    offer     = var.image["offer"]
-    sku       = var.image["sku"]
+    publisher = each.value["image"]["publisher"]
+    offer     = each.value["image"]["offer"]
+    sku       = each.value["image"]["sku"]
     version   = "latest"
   }
 
+  storage_os_disk {
+    name              = each.value["os_disk_name"]
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = each.value["managed_disk_type"]
+    disk_size_gb      = each.value["root_disk_size"]
+  }
+
   os_profile {
-    computer_name  = "node${count.index + 1}"
+    computer_name  = each.key
     admin_username = "azure"
-    custom_data = data.template_cloudinit_config.node_config[count.index].rendered
+    custom_data = each.value["user_data"]
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
     dynamic "ssh_keys" {
-      for_each = var.public_keys
+      for_each = each.value["public_keys"]
       iterator = key
       content {
         key_data = key.value

gcp/infrastructure.tf
@@ -191,27 +191,46 @@ resource "google_compute_instance" "login" {
   }
 }
 
+locals {
+  node_map = {
+    for key in keys(local.node):
+      key => merge(
+        {
+          name           = format("%s-%s", var.cluster_name, key),
+          project        = var.project,
+          zone           = local.zone,
+          image          = var.image,
+          root_disk_size = var.root_disk_size,
+          user_data      = data.template_cloudinit_config.node_config[key].rendered,
+          gpu_type       = "",
+          gpu_count      = 0
+        },
+        local.node[key]
+    )
+  }
+}
+
 resource "google_compute_instance" "node" {
-  count        = var.instances["node"]["count"]
-  project      = var.project
-  zone         = local.zone
-  name         = format("%s-node%d", var.cluster_name, count.index + 1)
-  machine_type = var.instances["node"]["type"]
+  for_each     = local.node_map
+  project      = each.value["project"]
+  zone         = each.value["zone"]
+  name         = each.value["name"]
+  machine_type = each.value["type"]
   scheduling {
     # Instances with guest accelerators do not support live migration.
-    on_host_maintenance = var.gpu_per_node["count"] > 0 ? "TERMINATE" : "MIGRATE"
+    on_host_maintenance = each.value["gpu_count"] > 0 ? "TERMINATE" : "MIGRATE"
   }
   guest_accelerator {
-    type  = var.gpu_per_node["type"]
-    count = var.gpu_per_node["count"]
+    type  = each.value["gpu_type"]
+    count = each.value["gpu_count"]
   }
   tags = ["node"]
 
   boot_disk {
     initialize_params {
-      image = var.image
+      image = each.value["image"]
       type  = "pd-ssd"
-      size  = var.root_disk_size
+      size  = each.value["root_disk_size"]
     }
   }
 
@@ -221,7 +240,7 @@ resource "google_compute_instance" "node" {
 
   metadata = {
     enable-oslogin     = "FALSE"
-    user-data          = data.template_cloudinit_config.node_config[count.index].rendered
+    user-data          = each.value["user_data"]
     user-data-encoding = "base64"
     VmDnsSetting       = "ZonalOnly"
   }

common/data.tf
@@ -1,5 +1,22 @@
 locals {
   domain_name = "${lower(var.cluster_name)}.${lower(var.domain)}"
+  node = {
+    for item in flatten([
+      for node in var.instances["node"]: [
+        for j in range(node.count): {
+          (
+            lookup(node, "prefix", "") != "" ?
+            format("%s-node%d", lookup(node, "prefix", ""), j+1) :
+            format("node%d", j+1)
+          ) = {
+            for key in setsubtract(keys(node), ["prefix", "count"]):
+              key => node[key]
+          }
+        }
+      ]
+    ]):
+    keys(item)[0] => values(item)[0]
+  }
 }
 
 resource "random_string" "munge_key" {
@@ -109,7 +126,7 @@ EOF
 }
 
 data "template_cloudinit_config" "node_config" {
-  count = var.instances["node"]["count"]
+  for_each = local.node
   part {
     filename     = "node.yaml"
     merge_type   = "list(append)+dict(recurse_array)+str()"
@@ -117,7 +134,7 @@ data "template_cloudinit_config" "node_config" {
     content      = templatefile(
       "${path.module}/cloud-init/puppet.yaml",
       {
-        node_name             = format("node%d", count.index + 1),
+        node_name             = each.key,
         sudoer_username       = var.sudoer_username,
         ssh_authorized_keys   = var.public_keys,
         puppetmaster          = local.mgmt1_ip,

common/variables.tf
@@ -9,7 +9,7 @@ variable "instances" {
   type = object({
     mgmt=object({type=string, count=number}),
     login=object({type=string, count=number}),
-    node=object({type=string, count=number}),
+    node=list(map(any)),
   })
 }
 

examples/aws/main.tf
@@ -13,7 +13,7 @@ module "aws" {
   instances = {
     mgmt  = { type = "t3.large",  count = 1 },
     login = { type = "t3.medium", count = 1 },
-    node  = { type = "t3.medium",  count = 1 }
+    node  = [{ type = "t3.medium",  count = 1 }]
   }
 
   storage = {

examples/azure/main.tf
@@ -21,7 +21,9 @@ module "azure" {
   instances = {
     mgmt  = { type = "Standard_DS2_v2", count = 1 },
     login = { type = "Standard_DS1_v2", count = 1 },
-    node  = { type = "Standard_DS1_v2", count = 1 }
+    node  = [
+      { type = "Standard_DS1_v2", count = 1 },
+    ]
   }
 
   storage = {

commit 10919ffbf82a7a3d127f381d6befa107854ff872

Fix regression introduced in c09ea17de1441ad73a2fadc16cbd5768262f265c
OpenStack compute node flavor with a default root disk with
sufficient space could not be created because of the condition on
image_id value being always false.

openstack/infrastructure.tf
@@ -230,7 +230,7 @@ resource "openstack_compute_instance_v2" "node" {
   for_each = local.node_map
   name     = each.value["name"]
 
-  image_id = each.value["root_disk"] == [] ? each.value["image_id"] : null
+  image_id = length(each.value["root_disk"]) == 0 ? each.value["image_id"] : null
 
   flavor_name     = each.value["type"]
   key_pair        = openstack_compute_keypair_v2.keypair.name