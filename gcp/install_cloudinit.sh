afc6f7ce988ee3e59b95976246a70eb9c1794012
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-06-04 11:47:55 -0400

    Fix gcp install_cloudinit.sh to work withn CentOS 8
    Closes issue #85. It also means one less reboot when
    using CentOS 7, thus improving a bit the provisioning
    time.

--- gcp/install_cloudinit.sh
+++ gcp/install_cloudinit.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
-if yum -q list installed "cloud-init" >/dev/null 2>&1; then
-    true
-else
-    sudo yum -y install cloud-init
-    sudo reboot
-fi
 rm -f /etc/dhcp/dhclient.d/google_hostname.sh
+if [ ! -f /etc/cloud/cloud-init.disabled ]; then
+    if  [ ! -f /usr/bin/cloud-init ]; then
+        # Try to install cloud-init every 5 seconds for 12 times.
+        for i in $(seq 12); do
+            yum -y install cloud-init && break
+            sleep 5
+        done
+    fi
+    # Verify installation was successful
+    if  [ -f /usr/bin/cloud-init ]; then
+        systemctl disable cloud-init
+        touch /etc/cloud/cloud-init.disabled
+        cloud-init init --local
+        cloud-init init
+        cloud-init modules --mode=config
+        cloud-init modules --mode=final
+    else
+        echo "Problem installing cloud-init. Verify network connectivity and reboot."
+    fi
+elif [ -f /usr/bin/cloud-init ]; then
+    yum -y remove cloud-init
+fi
