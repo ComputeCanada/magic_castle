0072688a047cdfa0fec5a8365c4a877ec45e117b
arshul <arshulmohd123@gmail.com>
2020-07-01 13:51:36 +0530

    add puppet cvmfs rev

--- examples/openstack/main.tf
+++ examples/openstack/main.tf
@@ -10,7 +10,7 @@ module "openstack" {
   image        = "CentOS-7-x64-2019-07"
   nb_users     = 10
   puppetenv_git= "https://github.com/arshul/puppet-genpipes"
-
+  puppetenv_rev = "cvmfs"
   instances = {
     mgmt  = { type = "p4-4gb", count = 1 },
     login = { type = "p1-0.75gb", count = 1 },
