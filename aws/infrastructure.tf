f6ca78c5d0a77c9a4b14a0fccf78bd9016a58e16
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-07-07 11:28:02 -0400

    Activate VPC DNS support in AWS
    Fix issue #109

--- aws/infrastructure.tf
+++ aws/infrastructure.tf
@@ -28,6 +28,9 @@ locals {
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "${var.cluster_name}-vpc"
   }
