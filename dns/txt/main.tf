15df9045cd3eba8c37505096842c172fe34c490f
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-02-18 11:53:15 -0500

    Add txt dns module and docs

---
+++ dns/txt/main.tf
@@ -0,0 +1,21 @@
+module "record_generator" {
+  source         = "../record_generator"
+  name           = lower(var.name)
+  login_ips      = var.public_ip
+  rsa_public_key = var.rsa_public_key
+}
+
+resource "local_file" "dns_record" {
+    content     = <<EOT
+; Import this file in ${var.domain} DNS zone
+%{ for record in module.record_generator.records ~}
+${record.name}.${var.domain}.   1   IN  ${record.type}  %{ if record.value != null }${record.value}%{ else }${record.data["algorithm"]} ${record.data["type"]}  ${record.data["fingerprint"]}%{ endif }
+%{ endfor ~}
+EOT
+    filename = "${var.name}.${var.domain}.txt"
+    file_permission = "0600"
+}
+
+output "hostnames" {
+  value = distinct(compact([for record in module.record_generator.records : join(".", [record.name, var.domain]) if record.type == "A" ]))
+}
