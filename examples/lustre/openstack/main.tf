2ee77b5b13022d084145ee1334ca2aec3e7602e5
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-03-29 15:34:30 -0400

    Add an example for building a lustre cluster

---
+++ examples/lustre/openstack/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_version = ">= 0.14.5"
+}
+
+module "openstack" {
+  source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref=tags"
+  config_git_url = "https://github.com/MagicCastle/lustre-environment.git"
+  config_version = "main"
+
+  cluster_name = "lustre"
+  domain       = "calculquebec.cloud"
+  image        = "CentOS-7-x64-2020-09"
+
+  instances = {
+    puppet = { type = "p4-7.5gb", tags = ["puppet"] }
+    mds    = { type = "p2-3.75gb", tags = ["mds"], count = 1 }
+    oss    = { type = "p2-3.75gb", tags = ["oss"], count = 2 }
+    login = { type = "p2-3.75gb", tags = ["public"], count = 1 }
+  }
+
+  storage = {
+    mds = {
+      mdt0 = { size = 5 }
+    }
+    oss = {
+      ost0 = { size = 5 }
+      ost1 = { size = 5 }
+    }
+  }
+
+  public_keys = [file("~/.ssh/id_rsa.pub")]
+}
+
+#output "volume_per_instance" {
+# value = module.openstack.volume_per_instance
+#}
+
+#output "public_instances" {
+#  value = module.openstack.public_instances
+#}
+
+## Uncomment to register your domain name with CloudFlare
+module "dns" {
+  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=tags"
+  email            = "felix@calculquebec.ca"
+  name             = module.openstack.cluster_name
+  domain           = module.openstack.domain
+  public_instances = module.openstack.public_instances
+  ssh_private_key  = module.openstack.ssh_private_key
+  sudoer_username  = module.openstack.sudoer_username
+}
+
+## Uncomment to register your domain name with Google Cloud
+# module "dns" {
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud?ref=tags"
+#   email            = "you@example.com"
+#   project          = "your-project-id"
+#   zone_name        = "you-zone-name"
+#   name             = module.openstack.cluster_name
+#   domain           = module.openstack.domain
+#   public_instances = module.openstack.public_instances
+#   ssh_private_key  = module.openstack.ssh_private_key
+#   sudoer_username  = module.openstack.sudoer_username
+# }
+
+output "hostnames" {
+  value = module.dns.hostnames
+}
