ad68da937ba7ef24bb5f459d178d50a942c8d75d
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-01 14:23:44 -0400

    Update azure module to work with tags

--- examples/cc_cluster/azure/main.tf
+++ examples/cc_cluster/azure/main.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 0.13.4"
+  required_version = ">= 0.14.2"
 }
 
 module "azure" {
-  source         = "git::https://github.com/ComputeCanada/magic_castle.git//azure"
+  source         = "git::https://github.com/ComputeCanada/magic_castle.git//azure?ref=tags"
   config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
-  config_version = "main"
+  config_version = "tags"
 
   cluster_name = "phoenix"
   domain       = "calculquebec.cloud"
@@ -19,18 +19,17 @@ module "azure" {
   root_disk_size = 30
 
   instances = {
-    mgmt  = { type = "Standard_DS2_v2", count = 1 },
-    login = { type = "Standard_DS1_v2", count = 1 },
-    node  = [
-      { type = "Standard_DS1_v2", count = 1 },
-    ]
+    mgmt  = { type = "Standard_DS2_v2",  count = 1, tags = ["mgmt", "puppet", "nfs"] },
+    login = { type = "Standard_DS1_v2", count = 1, tags = ["login", "public", "proxy"] },
+    node  = { type = "Standard_DS1_v2",  count = 1, tags = ["node"] }
   }
 
   storage = {
-    type         = "nfs"
-    home_size    = 100
-    project_size = 50
-    scratch_size = 50
+    nfs = {
+      home     = { size = 10 }
+      project  = { size = 50 }
+      scratch  = { size = 50 }
+    }
   }
 
   public_keys = [file("~/.ssh/id_rsa.pub")]
@@ -43,46 +42,26 @@ module "azure" {
   location = "eastus"
 }
 
-output "sudoer_username" {
-  value = module.azure.sudoer_username
-}
-
-output "guest_usernames" {
-  value = module.azure.guest_usernames
-}
-
-output "guest_passwd" {
-  value = module.azure.guest_passwd
-}
-
-output "public_ip" {
-  value = module.azure.ip
-}
-
 ## Uncomment to register your domain name with CloudFlare
 # module "dns" {
-#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare"
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=tags"
+#   email            = "you@example.com"
 #   name             = module.azure.cluster_name
 #   domain           = module.azure.domain
-#   email            = "you@example.com"
-#   public_ip        = module.azure.ip
-#   login_ids        = module.azure.login_ids
-#   rsa_public_key   = module.azure.rsa_public_key
+#   public_instances = module.azure.public_instances
 #   ssh_private_key  = module.azure.ssh_private_key
 #   sudoer_username  = module.azure.sudoer_username
 # }
 
 ## Uncomment to register your domain name with Google Cloud
 # module "dns" {
-#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud"
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud?ref=tags"
 #   email            = "you@example.com"
 #   project          = "your-project-id"
 #   zone_name        = "you-zone-name"
 #   name             = module.azure.cluster_name
 #   domain           = module.azure.domain
-#   public_ip        = module.azure.ip
-#   login_ids        = module.azure.login_ids
-#   rsa_public_key   = module.azure.rsa_public_key
+#   public_instances = module.azure.public_instances
 #   ssh_private_key  = module.azure.ssh_private_key
 #   sudoer_username  = module.azure.sudoer_username
 # }
