1b3c39e286a0e81185a139b1fb18ae2c4d1ef72b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-01 15:42:01 -0400

    Bump terraform required version

--- dns/cloudflare/versions.tf
+++ dns/cloudflare/versions.tf
@@ -1,6 +1,6 @@
 
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
   required_providers {
     cloudflare = {
       source = "cloudflare/cloudflare"
--- examples/lustre/openstack/main.tf
+++ examples/lustre/openstack/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.14.5"
+  required_version = ">= 0.14.2"
 }
 
 module "openstack" {
