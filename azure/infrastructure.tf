3d537592e58f22e8485ea029111667ea9151ff1b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-12 13:24:52 -0400

    Move network related locals and resource to network.tf for azure

--- azure/infrastructure.tf
+++ azure/infrastructure.tf
@@ -16,75 +16,6 @@ resource "azurerm_resource_group" "group" {
   location = var.location
 }
 
-# Create virtual network
-resource "azurerm_virtual_network" "virtualNetwork" {
-  name                = "${var.cluster_name}_vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = local.resource_group_name
-}
-
-# Create subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.cluster_name}_subnet"
-  resource_group_name  = local.resource_group_name
-  virtual_network_name = azurerm_virtual_network.virtualNetwork.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create public IPs
-resource "azurerm_public_ip" "public" {
-  for_each            = local.instances
-  name                = format("%s-%s-public-ipv4", var.cluster_name, each.key)
-  location            = var.location
-  resource_group_name = local.resource_group_name
-  allocation_method   = contains(each.value.tags, "public") ? "Static" : "Dynamic"
-}
-
-# Create Network Security Group and rule
-resource "azurerm_network_security_group" "public" {
-  name                = "${var.cluster_name}_public-firewall"
-  location            = var.location
-  resource_group_name = local.resource_group_name
-
-  dynamic "security_rule" {
-    for_each = var.firewall_rules
-    iterator = rule
-    content {
-      name                       = rule.value.name
-      priority                   = (100 + rule.value.from_port) % 4096
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = title(rule.value.ip_protocol)
-      source_port_range          = "*"
-      destination_port_range     = "${rule.value.from_port}-${rule.value.to_port}"
-      source_address_prefix      = "*"
-      destination_address_prefix = rule.value.cidr
-    }
-  }
-}
-
-# Create network interface
-resource "azurerm_network_interface" "nic" {
-  for_each            = local.instances
-  name                = format("%s-%s-nic", var.cluster_name, each.key)
-  location            = var.location
-  resource_group_name = local.resource_group_name
-
-  ip_configuration {
-    name                          = format("%s-%s-nic_config", var.cluster_name, each.key)
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = azurerm_public_ip.public[each.key].id
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "public" {
-  for_each                  = { for x, values in local.instances : x => true if contains(values.tags, "public") }
-  network_interface_id      = azurerm_network_interface.nic[each.key].id
-  network_security_group_id = azurerm_network_security_group.public.id
-}
-
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "instances" {
   for_each              = local.instances
@@ -170,16 +101,11 @@ locals {
 locals {
   resource_group_name = var.azure_resource_group == "" ? azurerm_resource_group.group[0].name : var.azure_resource_group
 
-  public_ip = {
-    for x, values in local.instances : x => azurerm_public_ip.public[x].ip_address
-    if contains(values.tags, "public")
-  }
-  puppetmaster_ip = [for x, values in local.instances : azurerm_network_interface.nic[x].private_ip_address if contains(values.tags, "puppet")]
   puppetmaster_id = try(element([for x, values in local.instances : azurerm_linux_virtual_machine.instances[x].id if contains(values.tags, "puppet")], 0), "")
   all_instances = { for x, values in local.instances :
     x => {
-      public_ip = azurerm_public_ip.public[x].ip_address
-      local_ip  = azurerm_network_interface.nic[x].private_ip_address
+      public_ip = azurerm_public_ip.public_ip[x].ip_address
+      local_ip  = azurerm_network_interface.local_ip[x].private_ip_address
       tags      = values["tags"]
       id        = azurerm_linux_virtual_machine.instances[x].id
       hostkeys = {
