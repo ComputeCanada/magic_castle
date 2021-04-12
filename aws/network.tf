e14d7ae23f31b92b386fda4d6c4e7d011f5616b8
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-12 14:42:32 -0400

    Normalize aws networking resource names

--- aws/network.tf
+++ aws/network.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "network" {
   cidr_block = "10.0.0.0/16"
 
   enable_dns_support   = true
@@ -11,7 +11,7 @@ resource "aws_vpc" "vpc" {
 
 # Internet gateway to give our VPC access to the outside world
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.network.id
 }
 
 # Grant the VPC internet access by creating a very generic
@@ -19,13 +19,13 @@ resource "aws_internet_gateway" "gw" {
 # such that we route traffic to outside as a last resource for
 # any route that the table doesn't know about.
 resource "aws_route" "internet_access" {
-  route_table_id         = aws_vpc.vpc.main_route_table_id
+  route_table_id         = aws_vpc.network.main_route_table_id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.vpc.id
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.network.id
   cidr_block = "10.0.0.0/24"
   availability_zone = local.availability_zone
   map_public_ip_on_launch = true
@@ -37,7 +37,7 @@ resource "aws_subnet" "private_subnet" {
 
 resource "aws_security_group" "allow_out_any" {
   name   = "allow_out_any"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.network.id
 
   egress {
     from_port   = 0
@@ -51,7 +51,7 @@ resource "aws_security_group" "allow_in_services" {
   name = "allow_in_services"
 
   description = "Allows services traffic into login nodes"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = aws_vpc.network.id
 
   dynamic "ingress" {
     for_each = var.firewall_rules
@@ -72,7 +72,7 @@ resource "aws_security_group" "allow_in_services" {
 resource "aws_security_group" "allow_any_inside_vpc" {
   name = "allow_any_inside_vpc"
 
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.network.id
 
   egress {
     from_port   = 0
@@ -95,7 +95,7 @@ resource "aws_security_group" "allow_any_inside_vpc" {
 
 resource "aws_network_interface" "nic" {
   for_each        = local.instances
-  subnet_id       = aws_subnet.private_subnet.id
+  subnet_id       = aws_subnet.subnet.id
   security_groups = concat(
     [
       aws_security_group.allow_any_inside_vpc.id,
