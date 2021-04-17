5c6273c0061b1c57ed9d23f3a210b39d1a2dd5bb
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-16 21:54:39 -0400

    Format instance_config outputs.tf

--- common/instance_config/outputs.tf
+++ common/instance_config/outputs.tf
@@ -1,10 +1,10 @@
 output "user_data" {
-  value = local.user_data
+  value     = local.user_data
   sensitive = true
 }
 
 output "rsa_hostkeys" {
-  value = { for x, values in var.instances: x => tls_private_key.rsa_hostkeys[values["prefix"]].public_key_openssh }
+  value = { for x, values in var.instances : x => tls_private_key.rsa_hostkeys[values["prefix"]].public_key_openssh }
 }
 
 output "private_key" {
