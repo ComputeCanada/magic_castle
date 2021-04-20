9b98859c0eb383eac2c0d24500686e00ce48363b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-20 11:38:50 -0400

    Add example for aws spot instances

---
+++ examples/advanced/spot_instances/aws/main.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_version = ">= 0.14.2"
+}
+
+module "aws" {
+  source         = "../../../../aws"
+  config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
+  config_version = "main"
+
+  cluster_name = "phoenix"
+  domain       = "calculquebec.cloud"
+  image        = "ami-033e6106180a626d0" # CentOS 7 -  ca-central-1
+
+  instances = {
+    mgmt  = { type = "t3.large",  count = 1, tags = ["mgmt", "puppet", "nfs"] },
+    login = { type = "t3.medium", count = 1, tags = ["login", "public", "proxy"] },
+    node  = { 
+        type = "t3.medium",
+        count = 1,
+        tags = ["node", "spot"],
+    }
+  }
+
+  volumes = {
+    nfs = {
+      home     = { size = 10, type = "gp2" }
+      project  = { size = 50, type = "gp2" }
+      scratch  = { size = 50, type = "gp2" }
+    }
+  }
+
+  public_keys = [file("~/.ssh/id_rsa.pub")]
+
+  nb_users     = 10
+  # Shared password, randomly chosen if blank
+  guest_passwd = ""
+
+  # AWS specifics
+  region            = "ca-central-1"
+}
+
+output "accounts" {
+  value = module.aws.accounts
+}
+
+output "public_ip" {
+  value = module.aws.public_ip
+}
+
+## Uncomment to register your domain name with CloudFlare
+module "dns" {
+  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare"
+  email            = "felix@calculquebec.cloud"
+  name             = module.aws.cluster_name
+  domain           = module.aws.domain
+  public_instances = module.aws.public_instances
+  ssh_private_key  = module.aws.ssh_private_key
+  sudoer_username  = module.aws.accounts.sudoer.username
+}
+
+## Uncomment to register your domain name with Google Cloud
+# module "dns" {
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud"
+#   email            = "you@example.com"
+#   project          = "your-project-id"
+#   zone_name        = "you-zone-name"
+#   name             = module.aws.cluster_name
+#   domain           = module.aws.domain
+#   public_instances = module.aws.public_instances
+#   ssh_private_key  = module.aws.ssh_private_key
+#   sudoer_username  = module.aws.accounts.sudoer.username
+# }
+
+output "hostnames" {
+	value = module.dns.hostnames
+}
