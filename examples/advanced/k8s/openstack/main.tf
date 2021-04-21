62b80193f97fe4b0a8cbb49a05b8e9805d21ea48
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-04-21 07:38:55 -0400

    Create an example to spawn a k8s cluster

---
+++ examples/advanced/k8s/openstack/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_version = ">= 0.14.2"
+}
+
+module "openstack" {
+  source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack"
+  config_git_url = "https://github.com/MagicCastle/k8s-environment.git"
+  config_version = "main"
+
+  cluster_name = "k8s-os"
+  domain       = "computecanada.dev"
+  image        = "CentOS-7-x64-2020-03"
+
+  instances = {
+    master   = { type = "c2-7.5gb-31", tags = ["controller", "puppet", "public"], count = 1 }
+    replica  = { type = "c2-7.5gb-31", tags = ["controller"], count = 2 }
+    node     = { type = "c2-7.5gb-31", tags = ["worker"], count = 5 }
+  }
+
+  volumes = {
+    gfs = {
+      data = { size = 100, type = "volumes-ssd" }
+    }
+  }
+
+  public_keys = [file("~/.ssh/id_rsa.pub")]
+
+  # Magic Castle default firewall rules are too permissive
+  # for this example. The following restricts it to SSH only.
+  firewall_rules = [
+    {"name"="SSH", "from_port"=22, "to_port"=22, "ip_protocol"="tcp", "cidr"="0.0.0.0/0"},
+  ]
+}
+
+output "public_ip" {
+  value = module.openstack.public_ip
+}
+
+## Uncomment to register your domain name with CloudFlare
+# module "dns" {
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare"
+#   email            = "you@example.com"
+#   name             = module.openstack.cluster_name
+#   domain           = module.openstack.domain
+#   public_instances = module.openstack.public_instances
+#   ssh_private_key  = module.openstack.ssh_private_key
+#   sudoer_username  = module.openstack.accounts.sudoer.username
+# }
+
+## Uncomment to register your domain name with Google Cloud
+# module "dns" {
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/gcloud"
+#   email            = "you@example.com"
+#   project          = "your-project-id"
+#   zone_name        = "you-zone-name"
+#   name             = module.openstack.cluster_name
+#   domain           = module.openstack.domain
+#   public_instances = module.openstack.public_instances
+#   ssh_private_key  = module.openstack.ssh_private_key
+#   sudoer_username  = module.openstack.accounts.sudoer.username
+# }
+
+# output "hostnames" {
+#   value = module.dns.hostnames
+# }
