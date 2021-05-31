133226a3f25f18af4687748127c9b1a3b7390ee0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-05-31 14:27:40 -0400

    Cleanup versions.tf for OVH

--- ovh/versions.tf
+++ ovh/versions.tf
@@ -1,24 +1,4 @@
 
 terraform {
   required_version = ">= 0.14"
-  required_providers {
-    http = {
-      source = "hashicorp/http"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    openstack = {
-      source = "terraform-provider-openstack/openstack"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-  }
 }
