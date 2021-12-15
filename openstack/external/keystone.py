4c5eba58c399a9728e416b6ece7fbe85b4cdab4b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2021-12-15 12:16:47 -0500

    Improve openstack keystone.py external script
    The script was failing to correctly extract the
    auth_url when the cloud name included quotes in
    the yaml file. This commit introduces the possibility
    for the script to import yaml if it is available,
    otherwise it defaults on our custom but fragile parser.

--- openstack/external/keystone.py
+++ openstack/external/keystone.py
@@ -3,6 +3,13 @@ import os
 
 from os.path import expanduser, exists
 
+try:
+    from yaml import safe_load
+except ImportError:
+    yaml_avail = False
+else:
+    yaml_avail = True
+
 cloudmap = { }
 if 'OS_AUTH_URL' in os.environ:
     cloudmap['auth_url'] = os.environ['OS_AUTH_URL']
@@ -10,25 +17,31 @@ elif 'OS_CLOUD' in os.environ:
     clouds = expanduser("~/.config/openstack/clouds.yaml")
     if exists(clouds):
         cloud_name = os.environ['OS_CLOUD']
-        found = False
-        key = cloud_name + ':'
-        search_level = 0
-        with open(clouds) as file:
-            for line in file:
-                line_lstripped = line.lstrip()
-                if line_lstripped[:1] == '#':
-                    continue
-                cur_level = len(line) - len(line_lstripped)
-                if cur_level < search_level:
-                    break
-                line_stripped = line_lstripped.rstrip()
-                # Skip commented lines
-                if line_stripped == key:
-                    found = True
-                    search_level = cur_level + 1
-                elif found and line_stripped[:9] ==  'auth_url:':
-                    cloudmap['auth_url'] = line_stripped[9:].strip()
-                    break
+        if yaml_avail:
+            with open(clouds) as f:
+                content = safe_load(f)
+                if cloud_name in content['clouds']:
+                    cloudmap['auth_url'] = content['clouds'][cloud_name]['auth']['auth_url']
+        else:
+            found = False
+            key = cloud_name
+            search_level = 0
+            with open(clouds) as file:
+                for line in file:
+                    line_lstripped = line.lstrip()
+                    # Skip commented lines
+                    if line_lstripped[:1] == '#':
+                        continue
+                    cur_level = len(line) - len(line_lstripped)
+                    if cur_level < search_level:
+                        break
+                    line_stripped = line_lstripped.rstrip().rstrip(':').strip('"\'')
+                    if found and line_stripped[:9] ==  'auth_url:':
+                        cloudmap['auth_url'] = line_stripped[9:].strip()
+                        break
+                    elif line_stripped == key:
+                        found = True
+                        search_level = cur_level + 1
 
 if 'auth_url' in cloudmap:
     parsed_url = cloudmap['auth_url']
