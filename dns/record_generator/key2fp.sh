c14780f7cac736c569032b791592f41aa33cc3d3
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-04-27 10:44:53 -0400

    Define a bash script when python and add python2 option

--- dns/record_generator/key2fp.sh
+++ dns/record_generator/key2fp.sh
@@ -3,6 +3,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -x "$(command -v "python3")" ]; then
     python3 ${SCRIPT_DIR}/key2fp.py
+elif [ -x "$(command -v "python2")" ]; then
+    python2 ${SCRIPT_DIR}/key2fp.py
 elif [ -x "$(command -v "python")" ]; then
     python ${SCRIPT_DIR}/key2fp.py
 else
--- openstack/external/keystone.sh
+++ openstack/external/keystone.sh
@@ -3,9 +3,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -x "$(command -v "python3")" ]; then
     python3 ${SCRIPT_DIR}/keystone.py
+elif [ -x "$(command -v "python2")" ]; then
+    python2 ${SCRIPT_DIR}/keystone.py
 elif [ -x "$(command -v "python")" ]; then
     python ${SCRIPT_DIR}/keystone.py
 else
-    echo "Could not find Python in PATH"
-    exit 1
+    # Python could not be found that's... unfortunate
+    # We will have to do the job in shell script instead, ugh!
+    local s="${AUTH_URL/#*:\/\/}"
+    name="${s%%.*}"
+    echo "{ \"auth_url\" : \"${OS_AUTH_URL}\" , \"name\":  \"${name}\" }"
 fi
