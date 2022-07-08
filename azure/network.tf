f7577bb01f99e6010b37c0f607fa420d1b5404e8
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-07-08 13:56:44 -0400

    Fix azurerm_network_interface private_ip_address_allocation value
    Fix issue #217

--- azure/network.tf
+++ azure/network.tf
@@ -56,7 +56,7 @@ resource "azurerm_network_interface" "nic" {
   ip_configuration {
     name                          = format("%s-%s-nic_config", var.cluster_name, each.key)
     subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "dynamic"
+    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip[each.key].id
   }
 }
