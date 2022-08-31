9297226e88dbeb3ace87dabe48e5b939930ac6a0
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-08-31 12:37:52 -0400

    Fix gcp machine_type.sh

--- gcp/external/machine_type.sh
+++ gcp/external/machine_type.sh
@@ -2,9 +2,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -x "$(command -v "python3")" ]; then
-    python3 ${SCRIPT_DIR}/machine_type.py $@
+    cat - | python3 ${SCRIPT_DIR}/machine_type.py
 elif [ -x "$(command -v "python2")" ]; then
-    python2 ${SCRIPT_DIR}/machine_type.py $@
+    cat - | python2 ${SCRIPT_DIR}/machine_type.py
 elif [ -x "$(command -v "python")" ]; then
-    python ${SCRIPT_DIR}/machine_type.py $@
+    cat - | python ${SCRIPT_DIR}/machine_type.py
 fi
