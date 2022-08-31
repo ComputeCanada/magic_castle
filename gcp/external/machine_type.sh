6ab54e6027427a37bcee5a1406fb93c6e70df08e
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2022-08-31 13:12:22 -0400

    Revert "Fix gcp machine_type.sh"
    This reverts commit 9297226e88dbeb3ace87dabe48e5b939930ac6a0.

--- gcp/external/machine_type.sh
+++ gcp/external/machine_type.sh
@@ -2,9 +2,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [ -x "$(command -v "python3")" ]; then
-    cat - | python3 ${SCRIPT_DIR}/machine_type.py
+    python3 ${SCRIPT_DIR}/machine_type.py $@
 elif [ -x "$(command -v "python2")" ]; then
-    cat - | python2 ${SCRIPT_DIR}/machine_type.py
+    python2 ${SCRIPT_DIR}/machine_type.py $@
 elif [ -x "$(command -v "python")" ]; then
-    cat - | python ${SCRIPT_DIR}/machine_type.py
+    python ${SCRIPT_DIR}/machine_type.py $@
 fi
