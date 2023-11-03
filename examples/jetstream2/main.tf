7d94e93dc5caf9f3d1ceeb84858a2827b7edd53c
Edwin Skidmore <edwins@arizona.edu>
2023-11-03 10:12:31 -0700

    removed extra resource not needed

--- examples/jetstream2/main.tf
+++ examples/jetstream2/main.tf
@@ -158,18 +158,6 @@ module "openstack" {
   sudoer_username = var.username
 }
 
-resource "null_resource" "ssh-agent" {
-
-    triggers = {
-        always_run = "${timestamp()}"
-    }
-
-    provisioner "local-exec" {
-        command = "eval `ssh-agent`; ssh-add"
-    }
-}
-
-
 data "openstack_compute_keypair_v2" "kp" {
   count = var.keypair == "" ? 0 : 1
   name = var.keypair
