9f5353c5114c00823150cfb4ff53d3df8e8b7337
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2023-12-21 09:34:05 -0500

    Move generate_terraform_self.rb to scripts/

--- common/provision/main.tf
+++ common/provision/main.tf
@@ -47,7 +47,7 @@ resource "terraform_data" "deploy_hieradata" {
       "sudo install -o root -g 52 -m 640 terraform_data.yaml user_data.yaml /etc/puppetlabs/data/",
       "sudo install -o root -g 52 -m 640 terraform_facts.yaml /etc/puppetlabs/facts/",
       "rm -f terraform_data.yaml user_data.yaml terraform_facts.yaml",
-      "[ -f /etc/puppetlabs/code/environments/production/generate_terraform_self.rb ] && sudo /etc/puppetlabs/code/environments/production/generate_terraform_self.rb || true",
+      "GEN_TF=/etc/puppetlabs/code/environments/production/scripts/generate_terraform_self.rb [ -f $GEN_TF ] && sudo $GEN_TF  || true",
       "[ -f /usr/local/bin/consul ] && [ -f /usr/bin/jq ] && consul event -token=$(sudo jq -r .acl.tokens.agent /etc/consul/config.json) -name=puppet $(date +%s) || true",
     ]
   }
