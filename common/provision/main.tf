757dd4f1408c2f0edf47abd528bda010ce1dbf68
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-05-03 11:21:52 -0400

    Fix issue #260
    Remove generation of private_key.pkcs7.pem from puppet.yaml

--- common/provision/main.tf
+++ common/provision/main.tf
@@ -5,6 +5,7 @@ variable "terraform_facts" { }
 variable "hieradata" { }
 variable "sudoer_username" { }
 variable "tf_ssh_key" { }
+variable "eyaml_key" { }
 
 resource "terraform_data" "deploy_hieradata" {
   for_each = length(var.bastions) > 0  ? var.puppetservers : { }
@@ -40,13 +41,23 @@ resource "terraform_data" "deploy_hieradata" {
     destination = "user_data.yaml"
   }
 
+  provisioner "file" {
+    content     = var.eyaml_key
+    destination = "private_key.pkcs7.pem"
+  }
+
   provisioner "remote-exec" {
     inline = [
-      "sudo mkdir -p /etc/puppetlabs/data /etc/puppetlabs/facts",
+      "sudo mkdir -p /etc/puppetlabs/data /etc/puppetlabs/facts /etc/puppetlabs/puppet/eyaml",
       # puppet user and group have been assigned the reserved UID/GID 52
       "sudo install -o root -g 52 -m 640 terraform_data.yaml user_data.yaml /etc/puppetlabs/data/",
       "sudo install -o root -g 52 -m 640 terraform_facts.yaml /etc/puppetlabs/facts/",
-      "rm -f terraform_data.yaml user_data.yaml terraform_facts.yaml",
+      # install the private key if it is a non-empty file
+      "test -s private_key.pkcs7.pem && sudo install -o 52 -g 52 -m 400 private_key.pkcs7.pem /etc/puppetlabs/puppet/eyaml",
+      # generate the public key X509 certificate from the private key file
+      # this is necessary to decrypt in hiera-eyaml 3.4.0
+      "sudo openssl req -new -key private_key.pkcs7.pem  -set_serial 1 -batch -out /etc/puppetlabs/puppet/eyaml/public_key.pkcs7.pem",
+      "rm -f terraform_data.yaml user_data.yaml terraform_facts.yaml private_key.pkcs7.pem",
       "[ -f /usr/local/bin/consul ] && [ -f /usr/bin/jq ] && consul event -token=$(sudo jq -r .acl.tokens.agent /etc/consul/config.json) -name=puppet $(date +%s) || true",
     ]
   }
--- gcp/infrastructure.tf
+++ gcp/infrastructure.tf
@@ -43,6 +43,7 @@ module "provision" {
   terraform_facts = module.configuration.terraform_facts
   hieradata       = var.hieradata
   sudoer_username = var.sudoer_username
+  eyaml_key       = var.eyaml_key
   depends_on      = [ google_compute_instance.instances ]
 }
 
