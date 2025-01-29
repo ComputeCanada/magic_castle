c0947544df05dc06ac81d973c2be6b11dd23974f
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2025-01-29 08:52:52 -0500

    Define an upper-bound for cloudflare version

--- dns/cloudflare/versions.tf
+++ dns/cloudflare/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 4.39.0"
+      version = ">= 4.39.0, < 5.0.0"
     }
     tls = {
       source = "hashicorp/tls"
