7529564869b1f3eb4c63df382251fbb6b4f9edab
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2025-07-09 14:22:33 -0400

    Add cloudflare dns to incus example

--- examples/incus/main.tf
+++ examples/incus/main.tf
@@ -25,6 +25,10 @@ module "incus" {
   nb_users = 10
   # Shared password, randomly chosen if blank
   guest_passwd = ""
+
+  # Set to true to make port 80 and 443 of the proxy container forwarded on the host
+  # There is a maximum of 1 cluster with forward_proxy = true per incus server.
+  forward_proxy = false
 }
 
 output "accounts" {
@@ -38,3 +42,21 @@ output "project" {
 output "public_ip" {
   value = module.incus.public_ip
 }
+
+
+# data "http" "agent_ip" {
+#   url = "http://ipv4.icanhazip.com"
+# }
+# locals {
+#   public_instances = { for host, values in module.incus.public_instances: host => merge(values, { "public_ip" = chomp(data.http.agent_ip.response_body) }) }
+# }
+# module "dns" {
+#   source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare"
+#   name             = module.incus.cluster_name
+#   domain           = module.incus.domain
+#   public_instances = local.public_instances
+# }
+#
+# output "hostnames" {
+#   value = module.dns.hostnames
+# }
