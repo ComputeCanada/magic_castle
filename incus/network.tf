4b52ad49be5f2401f8c12207add27339dd6cb6f6
etienned <etienne.dubeau@calculquebec.ca>
2025-07-18 10:22:23 -0400

    WIP Incus cluster support
    Update cluster for OVN
    
    Add support for static ips
    
    Fix var
    
    Update DNS
    
    Update network

--- incus/network.tf
+++ incus/network.tf
@@ -1,8 +1,17 @@
 resource "incus_network" "network" {
   name    = incus_project.project.name
   project = incus_project.project.name
-  config = {
-    "ipv4.nat"     = "true"
-    "ipv6.address" = "none"
-  }
+  type    = var.network_type
+  config = merge(
+    {
+      "ipv6.address" = "none"
+      "ipv4.nat"     = true
+    },
+    var.network_type == "ovn" ? {
+      "network"         = var.ovn_uplink_network
+      "ipv4.address"    = var.ovn_subnet
+      "dns.nameservers" = "1.1.1.1,1.0.0.1" # The default DNS server (incus) is inaccessible from OVN network
+    } : {}
+  )
 }
+
