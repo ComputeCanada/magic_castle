16041b844f8fdd2d58f04823c085204066565573
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2026-02-05 14:20:11 -0500

    Fix issue 393

--- dns/cloudflare/versions.tf
+++ dns/cloudflare/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 4.39.0, < 5.0.0"
+      version = ">= 5.16.0, < 6.0.0"
     }
     tls = {
       source = "hashicorp/tls"
